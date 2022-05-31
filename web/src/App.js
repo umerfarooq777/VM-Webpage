b5833749dbbaa062a521d7dcaf0e28f962fb9b3e
Umer Farooq <umerfarooq03100276796@gmail.com>
2022-05-31 07:49:54 +0000

    all done

---
+++ web/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>GROUP 18 React Web</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+       
+        <p>
+          Rizwan Iqbal Khan EB 19103103
+        </p><p>
+          Mahinder Shamji EB 19103041
+        </p><p>
+          Umer Farooq EB 19103136
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
