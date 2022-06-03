11eaabc39f088e5028a669b2ef4f39a152d08a65
Umer Farooq <umerfarooq03100276796@gmail.com>
2022-06-03 21:09:46 +0000

    spline added

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Spline from '@splinetool/react-spline';
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
+      <div className="d3">
+      <Spline scene="https://prod.spline.design/R2mzuDT1eUPUQUMq/scene.splinecode" />
+  
+      
+      </div>
+      {/* <header className="App-header">
       <h1>GROUP 18 React Web<br/>
         Azure Virtual Machine
       </h1>
@@ -18,9 +24,26 @@ function App() {
           Umer Farooq EB 19103136
         </p>
         
-      </header>
+      </header> */}
+
+      
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+// export default function App() {
+//   return (
+//     <Spline scene="https://prod.spline.design/Nbh3bhOhWse1LlzK/scene.splinecode" />
+//   );
+// }
